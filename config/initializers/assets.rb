8940bc3100176f0ba46790d6479d545240aa96ee
David Gecek <david.gecek@fiveminutes.eu>
2017-05-02 23:42:07 +0200

    working on design

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,11 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( business-casual.css )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
